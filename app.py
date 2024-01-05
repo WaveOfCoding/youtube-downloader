a84635ba0debf20f9ae455aa7141e4f2a3b91a64
WaveOfCoding <waveformwstcrz@gmail.com>
2024-01-05 17:15:37 +0300

    feature: create a resolution combo box

--- app.py
+++ app.py
@@ -4,26 +4,26 @@ from pytube import YouTube
 import os
 
 
+def download_video():
+    print("Clicked")
+
+
 # create a root window
 root = ctk.CTk()
 ctk.get_appearance_mode()
 ctk.set_default_color_theme("blue")
 
-
 # title of the window
 root.title("Youtube Downloader!")
-
 
 # set min and max width and height
 root.geometry("720x480")
 root.minsize(720, 480)
 root.maxsize(1080, 720)
 
-
 # create a frame to hold the content
 content_frame = ctk.CTkFrame(root)
 content_frame.pack(fill=ctk.BOTH, expand=True, padx=10, pady=10)
-
 
 # create a label and the entry widget for the video url
 url_label = ctk.CTkLabel(content_frame, text="Enter the youtube url here : ")
@@ -31,13 +31,16 @@ entry_url = ctk.CTkEntry(content_frame, width=400, height=40)
 url_label.pack(pady=10)
 entry_url.pack(pady=10)
 
-
 # create a download button
-download_button = ctk.CTkButton(content_frame, text="Download")
+download_button = ctk.CTkButton(content_frame, text="Download", command=download_video)
 download_button.pack(pady=10)
 
+# create a resolution combo box
+resolutions = ["720px", "360px", "240px"]
+resolution_var = ctk.StringVar()
+resolution_combobox = ttk.Combobox(content_frame, values=resolutions, textvariable=resolution_var)
+resolution_combobox.pack(pady=10)
+resolution_combobox.set("720px")
 
 # to start the app
 root.mainloop()
-
-
