df70cf4bba3f6e58479285f5a69c12c46066f3d6
WaveOfCoding <waveformwstcrz@gmail.com>
2024-01-05 16:11:05 +0300

    feature: create a root window

---
+++ app.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk
+from tkinter import ttk
+from pytube import YouTube
+import os
+
+
+# create a root window
+root = ctk.CTk()
+ctk.get_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+
+
