5d66f29e1b83852743e8987b2e9e8e8d42cf564f
WaveOfCoding <waveformwstcrz@gmail.com>
2024-01-05 16:15:31 +0300

    feature: set min and max width and height

--- app.py
+++ app.py
@@ -10,4 +10,13 @@ ctk.get_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
 
+# title of the window
+root.title("Youtube Downloader!")
+
+
+# set min and max width and height
+root.geometry("720x480")
+root.minsize(720, 480)
+root.maxsize(1080, 720)
+
 
